96689d0d01d294812340d98641abbdde1066db64
Embalo <kempesaguiar@gmail.com>
2020-05-20 15:36:21 -0300

    finish bug

--- src/main/java/br/com/kca/springboot/eventos/springbooteventosapp/controllers/AlunoController.java
+++ src/main/java/br/com/kca/springboot/eventos/springbooteventosapp/controllers/AlunoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.kca.springboot.eventos.springbooteventosapp.models.Aluno;
 import br.com.kca.springboot.eventos.springbooteventosapp.repository.EventoRepository;
@@ -31,5 +32,7 @@ public class AlunoController {
 		
 		return "redirect:/cadastrarEvento";
 	}
+	
+	
 
 }
